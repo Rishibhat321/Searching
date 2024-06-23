1d18dcc3812fb982acab71f93ae48eadc51b9e0d
Rishib#321 <rishitab2209@gmail.com>
2024-06-23 22:16:56 +0530

    Add initial files

---
+++ src/linear_search.java
@@ -0,0 +1,25 @@
+public class linear_search {
+    public static boolean linearSearch(int[] arr, int x){
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==x){
+                return true;
+            }
+        }
+
+        return false;
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {4,2,9,45,10,6};
+        int ele = 10;
+        boolean res = linearSearch(arr,ele);
+
+        if(res){
+            System.out.println("Element is present");
+        }
+        else{
+            System.out.println("Element is not present");
+        }
+    }
+}
