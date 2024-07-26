bc57995501609ea453f2037e02188b08dcd23e97
Rishib#321 <rishitab2209@gmail.com>
2024-07-26 20:20:30 +0530

    Add initial files

---
+++ src/SearchInASortedAndRotatedArray_BinarySearch.java
@@ -0,0 +1,55 @@
+
+public class SearchInASortedAndRotatedArray_BinarySearch {
+
+    public static int search(int[] arr, int key) {
+
+        int low = 0;
+        int high = arr.length-1;
+
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+
+            if(arr[mid]==key){
+                return mid;
+            }
+
+            if(arr[mid]>=arr[low])
+            {
+                if(key>=arr[low] && key<arr[mid]){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+
+            else{
+                if(key>arr[mid] && key<=arr[high]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+        }
+
+
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 7, 8, 9, 10, 1, 2, 3};
+        int key = 10;
+
+        int res = search(arr,key);
+
+        if(res==-1){
+            System.out.println("Element is not present in the array");
+        }
+        else{
+            System.out.println("Index of the element is: " + res);
+        }
+
+
+    }
+}
